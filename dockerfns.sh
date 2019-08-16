51136557b9e45da200f3733b84187a7b0eb72103
Richard Mortier <mort@cantab.net>
2019-08-16 16:49:10 +0100

    sh: sort out naming
    Signed-off-by: Richard Mortier <mort@cantab.net>

--- dockerfns.sh
+++ dockerfns.sh
@@ -15,68 +15,101 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place - Suite 330, Boston, MA 02111-1307, USA.
 
-function docker-kill {
-    docker ps --all --quiet | xargs docker stop -t 2
-    docker ps --all --quiet | xargs docker rm
+docker-kill() {
+  docker ps --all --quiet | xargs docker stop -t 2
+  docker ps --all --quiet | xargs docker rm
 }
 
-function docker-clean {
-    docker images | grep "<none>" | tr -s " " | cut -d" " -f 3 \
-        | xargs docker rmi
+docker-clean() {
+  docker images | grep "<none>" | tr -s " " | cut -d" " -f 3 \
+    | xargs docker rmi
 }
 
-function d {
-    docker run --interactive --tty --rm \
-           --volume "$(pwd -P)":/cwd \
-           --workdir /cwd \
-           $DARGS "$@"
+dr() {
+  docker run --interactive --rm \
+         --volume "$(pwd -P)":/cwd \
+         --workdir /cwd \
+         $DARGS "$@"
+}
+drtty() {
+  dr --tty "$@"
 }
 
 ## base distros
 
-function alpine {
-    if [ -z "$@" ] ; then
-        d alpine /bin/sh
-    else
-        d alpine /bin/sh -c "$*"
-    fi
+alpine() {
+  if [ -z "$*" ] ; then
+    drtty mor1/alpine /bin/sh
+  else
+    drtty mor1/alpine /bin/sh -c "$*"
+  fi
 }
 
-function debian {
-    if [ -z "$@" ] ; then
-        d debian:latest bash
-    else
-        d debian:latest bash -c "$*"
-    fi
+centos() {
+  if [ -z "$*" ] ; then
+    drtty centos:latest bash
+  else
+    drtty centos:latest bash -c "$*"
+  fi
 }
 
-function ubuntu {
-    if [ -z "$@" ] ; then
-        d ubuntu:latest bash
-    else
-        d ubuntu:latest bash -c "$*"
-    fi
+debian() {
+  if [ -z "$*" ] ; then
+    drtty debian:latest bash
+  else
+    drtty debian:latest bash -c "$*"
+  fi
+}
+
+opensuse() {
+  if [ -z "$*" ] ; then
+    drtty opensuse:latest bash
+  else
+    drtty opensuse:latest bash -c "$*"
+  fi
+}
+
+ubuntu() {
+  if [ -z "$*" ] ; then
+    drtty ubuntu:latest bash
+  else
+    drtty ubuntu:latest bash -c "$*"
+  fi
+}
+
+ubuntu14.04() {
+  if [ -z "$*" ] ; then
+    drtty ubuntu:14.04 bash
+  else
+    drtty ubuntu:14.04 bash -c "$*"
+  fi
 }
 
 ## containerised apps
 
-function casperjs  { d mor1/casperjs:latest         "$@" ;}
-function coffee    { d mor1/coffeescript:latest     "$@" ;}
-function dos2unix  { d mor1/dos2unix:latest         "$@" ;}
-function gawk      { d mor1/gawk:latest             "$@" ;}
-function ghi       { d mor1/ghi:latest              "$@" ;}
-function jekyll    { d mor1/jekyll:latest           "$@" ;}
-function jhead     { d mor1/jhead:latest            "$@" ;}
-function jq        { d mor1/jq:latest               "$@" ;}
-function lessc     { d mor1/lessc:latest            "$@" ;}
-function lilypond  { d markauskas/lilypond lilypond "$@" ;}
-function lynx      { d mor1/lynx:latest             "$@" ;}
-function pandoc    { d -v ~/.pandoc:/root/.pandoc mor1/pandoc:latest "$@" ;}
-function pdftk     { d mor1/pdftk:latest            "$@" ;}
-function phantomjs { d mor1/phantomjs:latest        "$@" ;}
-function poly      { d mor1/polyml:latest           "$@" ;}
-function python3   { d mor1/python3:latest          "$@" ;}
-function travis    { d mor1/travis:latest           "$@" ;}
-function unzip     { d mor1/unzip:latest            "$@" ;}
-function wget      { d mor1/wget:latest             "$@" ;}
-function yaml      { d mor1/ruamel:latest           "$@" ;}
+a2ps      () { dr mor1/a2ps:latest             "$@" ;}
+apkbuilder() { dr mor1/apkbuilder:latest       "$@" ;}
+casperjs  () { dr mor1/casperjs:latest         "$@" ;}
+coffee    () { dr mor1/coffeescript:latest     "$@" ;}
+dos2unix  () { dr mor1/dos2unix:latest         "$@" ;}
+gawk      () { dr mor1/gawk:latest             "$@" ;}
+ghi       () { dr mor1/ghi:latest              "$@" ;}
+jekyll    () { dr mor1/jekyll:latest           "$@" ;}
+jhead     () { dr mor1/jhead:latest            "$@" ;}
+jq        () { dr mor1/jq:latest -C            "$@" ;}
+lessc     () { dr mor1/lessc:latest            "$@" ;}
+lilypond  () { dr markauskas/lilypond lilypond "$@" ;}
+lynx      () { dr mor1/lynx:latest             "$@" ;}
+mutool    () { dr mor1/mutool:latest           "$@" ;}
+pandoc    () { dr -v ~/.pandoc:/root/.pandoc mor1/pandoc:latest "$@" ;}
+pdftk     () { dr mor1/pdftk:latest            "$@" ;}
+phantomjs () { dr mor1/phantomjs:latest        "$@" ;}
+polyml    () { dr mor1/polyml:latest           "$@" ;}
+python2   () { dr mor1/python2:latest          "$@" ;}
+pwclient  () { dr mor1/pwclient:latest         "$@" ;}
+python3   () { dr mor1/python3:latest          "$@" ;}
+tpaste    () { dr mor1/tpaste:latest           "$@" 2>/dev/null;}
+travis    () { dr mor1/travis:latest           "$@" ;}
+unzip     () { dr mor1/unzip:latest            "$@" ;}
+wget      () { dr mor1/wget:latest             "$@" ;}
+yaml      () { dr mor1/ruamel:latest           "$@" ;}
